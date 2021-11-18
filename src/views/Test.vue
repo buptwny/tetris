1570f67b6e0a9c552636f9e7d8c5d0fa2b910ea6
wuningyu <wuningyu@cloudstream-tech.com>
2021-11-18 19:03:50 +0800

    创建俄罗斯方块项目目录

---
+++ src/views/Test.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>hello {{word}}</h1>
+</template>
+
+<script lang="ts">
+    import { defineComponent , ref, reactive, onMounted } from 'vue';
+    const Test = defineComponent({
+        setup() {
+            let word: string = 'world'
+            return {
+                word
+            }
+        }
+    })
+    export default Test;
+</script>
+
+<style scoped>
+
+</style>
